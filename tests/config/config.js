4502daaa3780a915beddd294cf54e81c0b00c758
 artem <artem@artems-mbp.speedport.ip>
2024-06-12 20:01:22 +0200

    [main]: Test the job on ci

--- tests/config/config.js
+++ tests/config/config.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   baseUrl: 'https://api.github.com',
-  token: process.env.GITHUB_TOKEN,
+  token: process.env.MY_GITHUB_TOKEN,
   userAgent: 'request'
 };
